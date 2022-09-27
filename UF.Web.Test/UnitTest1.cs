3c57f876a6421277a0804d129e1afe099924afd9
Anh Nghia Nguyen <nghianghesi@gmail.com>
2022-09-26 23:30:40 -0500

    selenium integration

--- UF.Web.Test/UnitTest1.cs
+++ UF.Web.Test/UnitTest1.cs
@@ -1,12 +1,18 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using UF.Web.Controllers;
 using UF.Service;
-using UF.Web.Test.Mock;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Moq;
+using System.Reflection;
+using System.Collections.Generic;
+using UF.Entity;
 
 namespace UF.Web.Test;
 
@@ -14,24 +20,29 @@ namespace UF.Web.Test;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void Test1()
+    private Mock<IInventoryService> _inventoryService;
+    private WebApplication app;
+    private WebDriver driver;
+    [TestInitialize]
+    public void setup()
     {
-        var builder = WebApplication.CreateBuilder(    
-            new WebApplicationOptions() {
+        var builder = WebApplication.CreateBuilder(
+            new WebApplicationOptions()
+            {
                 WebRootPath = "C:\\Users\\nghia\\Source\\Repos\\web-unittest\\UF.Web\\wwwroot",
                 ContentRootPath = "C:\\Users\\nghia\\Source\\Repos\\web-unittest\\UF.Web\\wwwroot"
             }
         );
 
+        this._inventoryService = new Mock<IInventoryService>();
         // Add services to the container.
         builder.Services.AddControllersWithViews();
         builder.Services.AddMvc()
                 .AddApplicationPart(typeof(DemoController).Assembly)
                 .AddControllersAsServices();
-        builder.Services.AddSingleton<IInventoryService>(new InventoryServiceTest1Mock());
+        builder.Services.AddSingleton(this._inventoryService.Object);
 
-        var app = builder.Build();
+        this.app = builder.Build();
 
         app.UseExceptionHandler("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -48,9 +59,40 @@ public class UnitTest1
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
-        app.Run();
+        app.RunAsync();
+        this.driver = new ChromeDriver("."); // use PhantomJs if run in CICD
+    }
+
+    [TestMethod]
+    public async Task Test1()
+    {
+        DemoController ctrl = app.Services.GetService<DemoController>();
+        FieldInfo f = typeof(DemoController).GetField("inventoryService", BindingFlags.NonPublic | BindingFlags.Instance);
+        this._inventoryService.Setup(inv => inv.SearchInventory(It.IsAny<string>())).Returns(new List<Inventory>()
+        {
+            new Inventory()
+            {
+                Supplier = new Supplier() {Name="Test 1"},
+                Inventories = new List<PartInventory>()
+                {
+                    new PartInventory() {
+                        Part = new Part() { Number = "Test oem 1"},
+                        Amount = 3
+                    }
+                }
+            }
+        });
+        f.SetValue(ctrl, this._inventoryService.Object);
+
+        driver.Navigate().GoToUrl("https://localhost:5001/demo/index/abcd");
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        var element = wait.Until(driver => driver.FindElement(By.Id("demo-content")));
+        Assert.IsNotNull(element);
+        Assert.AreEqual(1, element.FindElements(By.ClassName("supplier-name")).Count);
 
 
-        System.Threading.Thread.Sleep(600000);
+
+        driver.Dispose();
+        await this.app.StopAsync(); // can't use TestCleanup as async doesn't supported.
     }
 }
